71e770d7aa55de0500a610acefb1a7a0b846efa2
Yarik2019 <yroslavterno@gmail.com>
2025-01-25 17:39:39 +0200

    add list contact

---
+++ src/components/ContactsList/ContactsList.jsx
@@ -0,0 +1,23 @@
+import { useSelector } from "react-redux";
+import UserItem from "../userItem/UserItem.jsx";
+import styles from "./ContactsList.module.css";
+import { selectFilteredUsers } from "../../redux/users/slice.js";
+
+const ContactsList = () => {
+  const users = useSelector(selectFilteredUsers);
+  return (
+    <div className={styles.blockList}>
+      {users.length ? (
+        <ul className={styles.usersList}>
+          {users.map((user) => (
+            <UserItem key={user.id} user={user} />
+          ))}
+        </ul>
+      ) : (
+        <p className={styles.text}>Нажаль пустий список</p>
+      )}
+    </div>
+  );
+};
+
+export default ContactsList;
