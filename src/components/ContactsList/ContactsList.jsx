9fb994dbf30438491cac3ab6779e710f65b64974
Yarik2019 <yroslavterno@gmail.com>
2025-01-25 17:42:52 +0200

    test fiexd

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,22 +1,23 @@
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 // import UserItem from "../userItem/UserItem.jsx";
 import styles from "./ContactsList.module.css";
-// import { selectFilteredUsers } from "../../redux/users/slice.js";
+import { selectFilteredUsers } from "../../redux/users/slice.js";
 
 const ContactsList = () => {
-  //   const users = useSelector(selectFilteredUsers);
+  const users = useSelector(selectFilteredUsers);
   return (
     <div className={styles.blockList}>
       dsadas
-      {/* {users.length ? (
+      {users.length ? (
         <ul className={styles.usersList}>
           {users.map((user) => (
-            <UserItem key={user.id} user={user} />
+            <div key={user.id}> dsadas</div>
+            // <UserItem key={user.id} user={user} />
           ))}
         </ul>
       ) : (
         <p className={styles.text}>Нажаль пустий список</p>
-      )} */}
+      )}
     </div>
   );
 };
